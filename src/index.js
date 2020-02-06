d94f6f6f3aed5b8f9f7c517e6bafa2e721bfdc46
preethiramiah <preethiramiah@gmail.com>
2020-02-06 07:02:58 +0530

    Loop through template folder and create images with given input text for each of the template

--- src/index.js
+++ src/index.js
@@ -6,35 +6,52 @@ const app = express();
 registerFont('../public/fonts/TradeGothicNextLTProBdCn.ttf', { family: 'Trade Gothic Next LT Pro BdCn' });
 
 
+const fontName = "Trade Gothic Next LT Pro BdCn";
+
+
 app.get('/', (req, res) => {    
-    const canvas = createCanvas(1124, 1124);
-    const ctx = canvas.getContext('2d');
     const inputText = process.argv[2] || "";
-    loadImage('../public/images/template1.png').then((image) => {
-        canvas.width = image.width;
-        canvas.height = image.height;
-        ctx.drawImage(image, 0, 0);
-        ctx.font = '100px "Trade Gothic Next LT Pro BdCn"';
-        fitText(ctx, inputText, 250, 0, canvas.width - 400, canvas.height - 200, 70);
-        canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'stats.png')));
-        const createImage = canvas.createJPEGStream().pipe(fs.createWriteStream(path.join(__dirname, 'stats.jpg')));
-        createImage.on("finish", function(){
-            res.download(path.join(__dirname, 'stats.jpg'));
-        });
+    const templatesFolder = '../public/images';
+    fs.readdir(templatesFolder, function(err, files){
+        if (err) {
+            console.error("Could not list the directory: ", err);
+        } else {
+            files.forEach(function(file, index){
+                loadImage(path.join(templatesFolder, file)).then((image) => {
+                    const canvas = createCanvas(1124, 1124);
+                    const ctx = canvas.getContext('2d');
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    ctx.drawImage(image, 0, 0);
+                    fitText(ctx, inputText, 300, 0, canvas.width, canvas.height, {fontSize: 100, fontColor: index === 1 ? "#000000" : "#ffffff"});
+                    canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'stats'+ index + '.png')));
+                    const createImage = canvas.createJPEGStream().pipe(fs.createWriteStream(path.join(__dirname, 'stats'+ index + '.jpg')));
+                    /* createImage.on("finish", function(){
+                        res.download(path.join(__dirname, 'stats.jpg'));
+                    }); */
+                });
+            });
+        }
     });
 });
  
 
-function fitText(ctx, text, x, y, width, height, fontSize) {
-    ctx.font = 'normal ' + fontSize + 'px Times New Roman';
-    var metrics = ctx.measureText(text);
-    
-    if (metrics.width <= width) {
-        ctx.fillText(text, x, y + height - fontSize/4);
+function fitText(ctx, text, x, y, width, height, fontProperties) {
+    const textWidth = width - 400;
+    const textHeight = height - 200;
+    const canvasSize = {width: width, height: height};
+    const textProperties = {fontSize: fontProperties.fontSize, fontName: fontProperties.fontName};
+    ctx.font = 'normal ' + fontProperties.fontSize + 'px ' + fontName;
+    ctx.fillStyle = fontProperties.fontColor;
+    var metrics = ctx.measureText(text);    
+    if (metrics.width <= textWidth) {
+        const yPosition = getYPositionOfText(ctx, text, 1, height);
+        ctx.fillText(text, x, yPosition);
+        textProperties.yPosition = yPosition;
+        textProperties.text = text;
+        addQuotes(ctx, textProperties, canvasSize);
         return;
     }
-    
-    // Wrap text
     var words = text.split(' '),
         line = '',
         lines = [];
@@ -42,9 +59,9 @@ function fitText(ctx, text, x, y, width, height, fontSize) {
     for(var n = 0; n < words.length; n++) {
         var testLine = line + words[n] + ' ';
         metrics = ctx.measureText(testLine);
-        if (metrics.width > width && n > 0) {
+        if (metrics.width > textWidth && n > 0) {
             lines.push(line);
-            line = words[n] + ' ';  // next line
+            line = words[n] + ' ';  
         }
         else {
             line = testLine;
@@ -52,34 +69,41 @@ function fitText(ctx, text, x, y, width, height, fontSize) {
     }
     lines.push(line);
     if (lines.length > 10) {
-        console.log('fontSize', fontSize);
-        return fitText(ctx, text, x, y, width, height, fontSize -1);
+        return fitText(ctx, text, x, y, width, height, fontProperties.fontSize -1);
     }
-    
-    var line_y = y + height - fontSize/4;
+    console.log("fontSize: " + fontProperties.fontSize);
+    let line_y = y + textHeight - fontProperties.fontSize/4;
+    if(lines && lines.length > 0){        
+        line_y = getYPositionOfText(ctx, lines[0], lines.length, height);
+        textProperties.endYPosition = line_y;
+    }
     for(var i=lines.length -1; i >= 0; i--) {
-        ctx.fillText(lines[i], x, line_y);
-        line_y -= fontSize * 1.1;
+        ctx.fillText(lines[i], x - 25, line_y);
+        line_y -= fontProperties.fontSize * 1.1;
     }
+    textProperties.yPosition = line_y;
+    textProperties.text = lines;
+    addQuotes(ctx, textProperties, canvasSize);
 }
 
-/* function wrapText(context, text, x, y, maxWidth, lineHeight){
-    const words = text.split(' ');
-    let line = '';
-    for(let n = 0; n < words.length; n++) {
-        const testLine = line + words[n] + ' ';
-        const metrics = context.measureText(testLine);
-        if (metrics.width > maxWidth && n > 0) {
-            context.fillText(line, x, y);
-            line = words[n] + ' ';
-            y += lineHeight;
-        }
-        else {
-            line = testLine;
-        }
-    }
-    context.fillText(line, x, y);
-} */
+function getYPositionOfText(ctx, text, numberOfLines, height){
+    const contentHeight = getTextHeight(ctx, text, numberOfLines);
+    console.log("y: ",contentHeight + (height - contentHeight)/2);
+    return contentHeight + (height - contentHeight)/2;
+}
+
+function getTextHeight(ctx, text, numberOfLines){
+    const metrics = ctx.measureText(text);
+    return (metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent) * numberOfLines;
+}
+
+function addQuotes(ctx, textProperties, canvasSize){
+    ctx.font = (textProperties.fontSize*2) + 'px ' + textProperties.fontName;
+    const beginQuoteYPosition = Array.isArray(textProperties.text) ? textProperties.yPosition + 50 : textProperties.yPosition - 100;
+    ctx.fillText("“", 200, beginQuoteYPosition);
+    const endQuoteYPosition = Array.isArray(textProperties.text) ? textProperties.endYPosition + 150 : textProperties.yPosition + 150;
+    ctx.fillText("”", canvasSize.width - 175, endQuoteYPosition);
+}
 
 
 app.listen(8000, () => {
